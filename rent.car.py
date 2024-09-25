28187501b5258d54f190cdd1332a490709cfe34d
Nícollas Carvalho <nicollaascarvalho@gmail.com>
2024-09-25 17:01:36 -0300

    Add files via upload

---
+++ rent.car.py
@@ -0,0 +1,5 @@
+# 09-rent-car.py = Escreva um programa que pergunte a quantidade de km
+# percorridos por um carro alugado pelo usuário,
+# assim como a quantidade de dias pelos quais o carro foi alugado.
+# Calcule o preço a pagar, sabendo que o
+# carro custa R$ 60 por dia e R$ 0,15 por km rodado.
